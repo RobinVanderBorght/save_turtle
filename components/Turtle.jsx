3a8bbab2ac6bc151648841fec843a9b6c306c6b2
Robin Van der Borght <Robin.VanderBorght@student.hogent.be>
2025-11-13 11:14:48 +0100

    Revision 2+ bubbles

--- components/Turtle.jsx
+++ components/Turtle.jsx
@@ -3,12 +3,11 @@ import { motion } from "framer-motion";
 
 const Turtle = React.memo(({ x, y }) => (
     <motion.div
-        className="absolute select-none"
+        className="absolute select-none z-20"
         animate={{ x, y }}
         transition={{ type: "tween", duration: 0.08 }}
         style={{
             fontSize: "6rem",
-            zIndex: 20,
             pointerEvents: "none",
             userSelect: "none",
         }}
